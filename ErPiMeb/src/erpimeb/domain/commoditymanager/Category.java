602be52e56fdcd162ed04bfac41999054ef9ea77
DANIEL-ACER\DanielToft <Dstoft@hotmail.com>
2017-05-18 13:24:21 +0200

    Added the filler methods with SQL for the databaseManager.

--- ErPiMeb/src/erpimeb/domain/commoditymanager/Category.java
+++ ErPiMeb/src/erpimeb/domain/commoditymanager/Category.java
@@ -5,6 +5,7 @@
  */
 package erpimeb.domain.commoditymanager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,7 +15,30 @@ import java.util.List;
 public class Category {
     
     private String name;
-    private List<String> subCategories;
+    private List<Category> subcategories;
     private List<String> tagList;
     private List<Product> productList;
+    
+    public Category() {
+        this.subcategories = new ArrayList<>();
+        this.tagList = new ArrayList<>();
+        this.productList = new ArrayList<>();
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void addSubcategory(Category category) {
+        this.subcategories.add(category);
+    }
+    
+    public void addTag(String tagName) {
+        this.tagList.add(name);
+    }
+    
+    public void addProduct(Product product) {
+        this.productList.add(product);
+    }
+    
 }
