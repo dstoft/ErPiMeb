71e490ed11ccecf87085b4871c705a75df421654
DANIEL-ACER\DanielToft <Dstoft@hotmail.com>
2017-06-02 01:20:47 +0200

    Final changes, to make sure that the program can be run from the databaseInfo file.

--- ErPiMeb/src/erpimeb/persistence/databasemanager/DatabaseManager.java
+++ ErPiMeb/src/erpimeb/persistence/databasemanager/DatabaseManager.java
@@ -72,69 +72,6 @@ public class DatabaseManager implements CommodityDatabaseManagerFacade, OrderDat
     private String password = "admin";
 
     private Connection conn = null;
-
-    public static void main(String[] args) {
-        DatabaseManager db = new DatabaseManager();
-        Scanner userInput = new Scanner(System.in);
-
-        Scanner fileInput;
-        try {
-            fileInput = new Scanner(new File("databaseInfo.txt"));
-        } catch (FileNotFoundException ex) {
-            System.out.println("Database info file not found.");
-            return;
-        }
-
-        System.out.println("Here you can change the database connection information to your own.");
-        System.out.println("We have put in our own default values. Change them until it matches yours.");
-
-        while (db.conn == null) {
-            System.out.println("Port: " + db.port);
-            System.out.println("Host: " + db.host);
-            System.out.println("Database Name: " + db.databaseName);
-            System.out.println("Username: " + db.username);
-            System.out.println("Password: " + db.password);
-
-            try {
-                db.conn = DriverManager.getConnection(db.url + db.host + ":" + db.port + "/" + db.databaseName, db.username, db.password);
-            } catch (SQLException ex) {
-                System.out.println("Connection information is invalid. Please edit the information.");
-            }
-            if (db.conn != null) {
-                try (BufferedWriter print = new BufferedWriter(new FileWriter(new File("databaseInfo.txt")))) {
-                    print.write("" + db.port);
-                    print.newLine();
-                    print.write(db.host);
-                    print.newLine();
-                    print.write(db.databaseName);
-                    print.newLine();
-                    print.write(db.username);
-                    print.newLine();
-                    print.write(db.password);
-                } catch (IOException ex) {
-                    System.out.println("Error writing to the database info file.");
-                    return;
-                }
-                System.out.println("Successfully established connection to database.");
-                return;
-            }
-
-            System.out.println("Set port: ");
-            db.port = Integer.parseInt(userInput.nextLine());
-
-            System.out.println("Set Host: ");
-            db.host = userInput.nextLine();
-
-            System.out.println("Set Database name: ");
-            db.databaseName = userInput.nextLine();
-
-            System.out.println("Set Username: ");
-            db.username = userInput.nextLine();
-
-            System.out.println("Set Password: ");
-            db.password = userInput.nextLine();
-        }
-    }
     
     @Override
     public boolean saveCustomer(Customer currentUser) {
